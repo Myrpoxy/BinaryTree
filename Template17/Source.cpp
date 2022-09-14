ca7790f2b62cef86f7e241fbb4dca6d4cf518dd1
User1-13 <User1-13@1-13>
2022-09-14 19:29:12 +0300

    Добавлен вложенный класс Node. Поля и конструктор класса BinaryTree.

--- Template17/Source.cpp
+++ Template17/Source.cpp
@@ -5,9 +5,19 @@ using namespace std;
 class BinaryTree
 {
 public:
+	BinaryTree() = default;
+	BinaryTree(int data) { root = new Node(data); }
 
 private:
-
+	class Node
+	{
+	public:
+		Node(int data) : data{ data }, left{ nullptr }, right{ nullptr } {}
+		Node* left;
+		Node* right;
+		int data;
+	};
+	Node* root;
 };
 
 
