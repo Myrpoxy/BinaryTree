5243c91ee1f35a66bc856e02fe8134771a26e720
User1-13 <User1-13@1-13>
2022-09-14 19:39:51 +0300

    Реализованы методы: insert и _del.
    добавлены методы print и _print для вывода дерева на консоль.

--- Template17/Source.cpp
+++ Template17/Source.cpp
@@ -7,17 +7,55 @@ class BinaryTree
 public:
 	BinaryTree() = default;
 	BinaryTree(int data) { root = new Node(data); }
+	~BinaryTree() { _del(root); }
+
+	void print() { _print(root); }
+	void insert(int data) { _insert(data, root); }
 
 private:
+
 	class Node
 	{
 	public:
 		Node(int data) : data{ data }, left{ nullptr }, right{ nullptr } {}
+		int data;
 		Node* left;
 		Node* right;
-		int data;
 	};
+
+
 	Node* root;
+	void _print(Node* branch)
+	{
+		if (branch != nullptr)
+		{
+			cout << branch->data << "\t";
+			_print(branch->left);
+			_print(branch->right);
+		}
+	}
+	void _insert(int data, Node*& branch) 
+	{
+		if (branch != nullptr)
+		{
+			if (data > branch->data)
+				_insert(data, branch->right);
+			else if (data < branch->data)
+				_insert(data, branch->left);
+		}
+		else
+			branch = new Node(data);
+	}
+
+	void _del(Node*& branch)
+	{
+		if (branch != nullptr)
+		{
+			_del(branch->left);
+			_del(branch->right);
+			delete branch;
+		}
+	}
 };
 
 
@@ -25,7 +63,13 @@ int main()
 {
 	setlocale(LC_ALL, "ru");
 	
+	BinaryTree tree;
 
+	tree.insert(5);
+	tree.insert(4);
+	tree.insert(7);
+
+	tree.print();
 
 	return 0;
 }
